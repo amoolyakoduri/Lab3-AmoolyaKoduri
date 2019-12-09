4c51c503a61ea09814599469a162fd098b89c9de
Amoolya <amoolyakoduri@gmail.com>
2019-12-08 23:48:00 -0800

    final fixes

--- grubfrontend/src/mutations/mutations.js
+++ grubfrontend/src/mutations/mutations.js
@@ -17,4 +17,63 @@ const signUpMutation = gql`
     }
 `;
 
-export {signUpMutation};
+const signUpRestMutation = gql`
+    mutation SignUpRest(
+        $name: String,
+        $ownerEmail: String,
+        $address : String,
+        $zip : String,
+        $cuisine : String) {
+        signUpRest(
+            name : $name,
+            ownerEmail : $ownerEmail,
+            address : $address ,
+            zip : $zip ,
+            cuisine : $cuisine )
+    }
+`;
+
+const updateUserDetailsMutation = gql`
+    mutation UpdateUserDetails(
+        $firstName: String ,
+        $lastName: String ,
+        $emailId: String,
+        $phone : String,
+        $address : String ) {
+        updateUserDetails(
+            firstName : $firstName ,
+            lastName : $lastName ,
+            emailId : $emailId ,
+            phone : $phone ,
+            address : $address )
+        }
+   `;
+
+const addSectionMutation =  gql`
+    mutation AddSection(
+        $ownerEmail : String,
+        $section : String  ) {
+        addSection(
+            ownerEmail : $ownerEmail ,
+            section : $section )
+    }
+`;
+
+const addItemMutation = gql`
+    mutation AddItem(
+        $ownerEmail :  String,
+        $name : String,
+        $desc : String,
+        $price : Integer,
+        $section : String) {
+            addItem(
+                ownerEmail : $ownerEmail,
+                name : $name ,
+                desc : $desc ,
+                price : $price ,
+                section : $section )
+        }
+    `;
+
+
+export {signUpMutation , signUpRestMutation, updateUserDetailsMutation, addSectionMutation, addItemMutation};
